792f074ce17c08d6a11fe6dda59b39078d04f70b
Dana <dana.tassler@gmail.com>
2011-02-27 14:03:00 -0600

    Removed default rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 SampleApp::Application.routes.draw do
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
   get "pages/home"
 
   get "pages/contact"
@@ -6,6 +11,9 @@ SampleApp::Application.routes.draw do
   get "pages/about"
 
   get "pages/help"
+
+  root :to => 'pages#home'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
